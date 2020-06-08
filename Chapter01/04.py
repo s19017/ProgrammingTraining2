abba04a02d12c0a6e3833c9c89fb66e5c5e75d49
yudai miyaguni <s19017@std.it-college.ac.jp>
2020-06-08 12:28:59 +0900

    first commit

--- Chapter01/04.py
+++ Chapter01/04.py
@@ -1,17 +1,11 @@
-text = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
-split = text.split()
-
-first_letter = [1, 5, 6, 7, 8, 9, 15, 16, 19]
-num = 1
-result = {}
-
-for i in split:
-    if num in first_letter:
-        result[i[0]] = num
-
+s = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+s = s.replace('.','')
+idx = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+mp = {}
+for i,w in enumerate(s.split()):
+    if (i+1) in idx:
+        v = w[:1]
     else:
-        result[i[0] + i[1]] = num
-    
-    num += 2
-    
-  print(result)
+        v = w[:2]
+    mp[v] = i+1
+print (mp)
